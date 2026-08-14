ce3e8d8c76e3eb54357ac10d844ccc68d1ff4fef
Mahmod-Mosad <mmsdf2006@gmail.com>
2026-08-14 21:23:28 +0300

    init project

---
+++ src/main/java/com/mahmoud/quickserve/QuickserveApplication.java
@@ -0,0 +1,14 @@
+package com.mahmoud.quickserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickserveApplication {
+
+	  public static void main(String[] args) {
+		SpringApplication.run(QuickserveApplication.class, args);
+	}
+
+
+}
